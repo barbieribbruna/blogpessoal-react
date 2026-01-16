fdcc8be4a23068b5435d62da4620904571fa3ce7
barbieribbruna <barbieribbruna@gmail.com>
2026-01-16 17:48:11 -0300

    melhorias componente login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 function Login() {
     return (
         <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen place-items-center font-bold bg-[var(--offwhite)]">
+            <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen place-items-center font-bold bg-[var(--azul-bemclaro)]">
 
                 {/* FORM (lado esquerdo) */}
-                <div className="w-full flex items-center justify-center px-6 py-10">
-                    <form className="w-full max-w-xl flex flex-col gap-5 bg-white/70 backdrop-blur-sm border border-slate-200 rounded-2xl shadow-sm p-10 lg:p-12">
-                        <header className="flex flex-col gap-3 mb-2">
-                            <h2 className="text-[var(--azul-escuro)] text-5xl leading-tight">
+                <div className="w-full lg:w-[50%] lg:h-[70%] flex items-center justify-center px-2 py-10">
+                    <form className="w-full p-10 lg:p-10 bg-white/60 backdrop-blur-sm rounded-[1.8rem] shadow-xl">
+                        <header className="flex flex-col gap-2">
+                            <h2 className="text-[var(--azul-escuro)] text-[4.8rem] leading-tight">
                                 Entrar
                             </h2>
 
-                            <p className="text-base font-medium text-slate-600 leading-snug">
+                            <p className="text-[2.4rem] text-justify font-medium text-slate-600 leading-tight mb-3">
                                 Acesse sua conta para acompanhar e publicar conteúdos no blog.
                             </p>
                         </header>
@@ -22,7 +22,7 @@ function Login() {
                         <div className="flex flex-col w-full gap-1">
                             <label
                                 htmlFor="usuario"
-                                className="text-[var(--azul-escuro)] text-lg"
+                                className="text-[var(--azul-escuro)] text-[2.2rem]"
                             >
                                 Usuário
                             </label>
@@ -31,7 +31,7 @@ function Login() {
                                 id="usuario"
                                 name="usuario"
                                 placeholder="Seu usuário ou e-mail"
-                                className="border-2 border-slate-300 rounded-xl p-4 text-[var(--azul-escuro)] font-medium placeholder:text-slate-400 focus:outline-none focus:border-[var(--azul-vivo)] focus:ring-2 focus:ring-[var(--azul-vivo)]/40"
+                                className="border-2 border-slate-300 rounded-xl p-5 text-[var(--azul-escuro)] text-[1.8rem] font-medium placeholder:text-slate-400 focus:outline-none focus:border-[var(--azul-vivo)] focus:ring-2 focus:ring-[var(--azul-vivo)]/40"
                             />
                         </div>
 
@@ -39,7 +39,7 @@ function Login() {
                         <div className="flex flex-col w-full gap-1">
                             <label
                                 htmlFor="senha"
-                                className="text-[var(--azul-escuro)] text-lg"
+                                className="text-[var(--azul-escuro)] text-[2.2rem]"
                             >
                                 Senha
                             </label>
@@ -48,25 +48,25 @@ function Login() {
                                 id="senha"
                                 name="senha"
                                 placeholder="Sua senha"
-                                className="border-2 border-slate-300 rounded-xl p-4 text-[var(--azul-escuro)] font-medium placeholder:text-slate-400 focus:outline-none focus:border-[var(--azul-vivo)] focus:ring-2 focus:ring-[var(--azul-vivo)]/40"
+                                className="border-2 border-slate-300 rounded-xl p-5 text-[var(--azul-escuro)] text-[1.8rem] font-medium placeholder:text-slate-400 focus:outline-none focus:border-[var(--azul-vivo)] focus:ring-2 focus:ring-[var(--azul-vivo)]/40"
                             />
                         </div>
 
                         {/* BOTÃO */}
                         <button
                             type="submit"
-                            className="rounded-xl bg-[var(--azul-vivo)] flex justify-center hover:opacity-90 text-white w-full sm:w-1/2 py-3 transition-opacity"
+                            className="rounded-xl text-white text-[1.8rem] bg-[var(--azul-vivo)] hover:opacity-70 w-2/2 py-3 flex justify-center transition-opacity cursor-pointer"
                         >
                             <span>Entrar</span>
                         </button>
 
                         <hr className="border-slate-300 w-full" />
 
-                        <p className="text-slate-700 font-medium">
+                        <p className="text-slate-700 text-[1.8rem] font-medium">
                             Ainda não tem uma conta?{" "}
                             <Link
                                 to="/cadastro"
-                                className="text-[var(--azul-escuro)] hover:underline"
+                                className="text-[var(--azul-escuro)] text-[1.8rem] hover:underline"
                             >
                                 Cadastre-se
                             </Link>
