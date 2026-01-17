a63a12d973c3721e128ac2758e71d383ab55c1a7
barbieribbruna <barbieribbruna@gmail.com>
2026-01-17 17:32:12 -0300

    adicionar AuthProvider na aplicação App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,29 @@ import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
     return (
         <div className="min-h-screen flex flex-col">
 
-            <BrowserRouter>
-                <Navbar />
-                <main className="flex-1">
-                    <Routes>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Navbar />
+                    <main className="flex-1">
+                        <Routes>
 
-                        <Route path="/" element={<Login />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/cadastro" element={<Cadastro />} />
+                            <Route path="/" element={<Login />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/cadastro" element={<Cadastro />} />
 
-                        {/* fallback opcional */}
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </BrowserRouter>
+                            {/* fallback opcional */}
+                            <Route path="*" element={<Navigate to="/" />} />
+                        </Routes>
+                    </main>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
 
         </div>
     );
